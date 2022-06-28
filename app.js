81c190528d720ed8cc69c02c9e3c02554e20a834
Mzala123 <justicemwanzamj@gmail.com>
2022-06-28 12:06:16 +0200

    cors initiation

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ const passport = require('passport');
 var app = express();
 
 // view engine setup
-app.use(cors())
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
