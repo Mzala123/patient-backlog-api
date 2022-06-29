490b0eb2176867c46078d94f994f683efdd19282
Mzala123 <justicemwanzamj@gmail.com>
2022-06-29 11:10:11 +0200

    11:10am

--- app.js
+++ app.js
@@ -22,12 +22,14 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(cors({origin: true}))
+
+app.use(cors())
+app.options('*', cors())
 
 app.use(logger('dev'));
 app.use(express.json());
-/*app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));*/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
