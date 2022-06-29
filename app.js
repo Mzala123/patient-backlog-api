a9aa06dcc606948f8889c374e674921ee28e6634
Mzala123 <justicemwanzamj@gmail.com>
2022-06-29 10:53:39 +0200

    cors

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 
-var app = express();
-app.use(cors())
+
 
 require('./model/db')
 require('./config/passport')
@@ -17,12 +16,13 @@ var routesApi = require('./routes/index');
 var usersRouter = require('./routes/users');
 const passport = require('passport');
 
-
+var app = express();
 
 // view engine setup
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(cors({origin: true}))
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,6 +31,7 @@ app.use(bodyParser.urlencoded({ extended: false }));*/
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.use(passport.initialize())
