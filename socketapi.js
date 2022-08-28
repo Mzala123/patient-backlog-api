fbd3b20201446886f182b90b099ff30da149834f
Mzala123 <justicemwanzamj@gmail.com>
2022-08-28 22:27:15 +0200

    chat trial done

--- socketapi.js
+++ socketapi.js
@@ -9,6 +9,14 @@ const socketapi = {
 
 io.on('connection', function(socket){
     console.log("A user mtende patient systems connected", socket.id)
+
+    socket.on('message', data=>{
+        socket.broadcast.emit('message:received', data)
+    })
+
+    socket.on('disconnect', ()=>{
+        console.log(`user ${socket.id} left`)
+    })
 })
 
 module.exports = socketapi
